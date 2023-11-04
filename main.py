2eb55066836dcfa570fd9844acb1c0d464e69175
ChristianHaase <christian.haase.93@gmail.com>
2023-11-04 19:05:12 +0100

    feat: Build, train and evaluate MNIST model

---
+++ main.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+# setup training variables
+learning_rate = 0.05
+epochs = 20
+
+# load MNIST dataset
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# setup and compile the network
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu),
+    tf.keras.layers.Dense(units=10)
+])
+
+model.build(x_train[0].shape)
+
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+model.summary()
+
+# train the model
+model.fit(x_train, y_train, epochs=epochs)
+model.evaluate(x_test, y_test)

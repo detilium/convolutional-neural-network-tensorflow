da7da929d9f8eb3d9f81122e7e7ce4d32d4514b9
ChristianHaase <christian.haase.93@gmail.com>
2023-11-04 19:24:14 +0100

    feat: Add functionality to visualize images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 # setup training variables
 learning_rate = 0.001
@@ -8,7 +9,21 @@ epochs = 6
 (x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+# visualize dataset
+'''
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i])
+    plt.xlabel(y_train[i])
+plt.show()
+'''
+
 # setup and compile the network
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu),
